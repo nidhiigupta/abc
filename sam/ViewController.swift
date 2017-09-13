0f205a39a2bad63a47281c102d919bec705c923f
student <student@admins-Mac-mini-8.local>
2017-09-13 16:06:15 +0530

    aa

--- sam/ViewController.swift
+++ sam/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     
      var ss = "aaaaaa"
-    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
